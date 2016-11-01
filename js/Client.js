b10a7a825e5225786fd0786a007da7160fec623f
Jack Rosenhauer II <jack@jrosedev.com>
2016-11-01 00:40:52 -0400

    client FTP

---
+++ js/Client.js
@@ -0,0 +1,38 @@
+/**
+ * Created by jack on 10/31/16.
+ */
+var Client = require('jsftp');
+
+var options = {
+    host: '127.0.0.1',
+    port: 7710,
+    user: 'jack',
+    pass: 'test',
+    cwd: '/'
+};
+
+module.exports = {
+    client: function(customOptions){
+        customOptions = customOptions || {};
+        Object.keys(options).forEach(function(key){
+            if (!customOptions.hasOwnProperty(key)){
+                customOptions[key] = options[key];
+            }
+        });
+
+        var client = new Client(customOptions);
+        client.auth(
+            customOptions.user,
+            customOptions.pass,
+            function(err, res){
+                if (err){
+                    console.log(err);
+                    return;
+                }
+                console.log(res);
+            }
+        );
+
+        return client;
+    }
+};
