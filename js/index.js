890ce31b810c10d2b4aa0b544679f2a6bf3aec73
Jack Rosenhauer II <jack@jrosedev.com>
2016-11-03 18:03:14 -0400

    formatting

--- js/index.js
+++ js/index.js
@@ -9,24 +9,17 @@ var path = require('path');
 var net = require('net');
 
 ipc.send('getPort');
-ipc.on('receivePort', function(port){
+ipc.on('receivePort', function (port) {
 
-
-  var hostServerInput = document.querySelector('input#host-address');
-  var connectButton = document.querySelector('button#connect');
-
-  connectButton.addEventListener('click', function(){
+  connectButton.addEventListener('click', function () {
     var hostServerAddress = hostServerInput.value;
     console.log(hostServerAddress);
     ipc.send('connect', hostServerAddress);
   });
 
-  var server = require("./js/Server").server({port: port});
-  var client = require("./js/Client").client({port: port});
-
-  setTimeout(function(){
-    client.list("/", function(err, res){
-      if (err){
+  setTimeout(function () {
+    client.list("/", function (err, res) {
+      if (err) {
         console.log(err);
         return;
       }
@@ -36,4 +29,10 @@ ipc.on('receivePort', function(port){
 
   }, 100);
 
+});
+
+// var server = require("./js/ftps").server({port: port});
+// var client = require("./js/ftpc").client({port: port});
+document.getElementById("connect").addEventListener("click", function(event){
+  console.log("!!!");
 });
